75d42783704ca7c5c051e300bd7e0e03aafa6232
swati037 <vettiyattilswati@gmail.com>
2022-03-27 15:31:57 +0530

    first

--- resume_builder/src/App.js
+++ resume_builder/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a resume builder project
         </a>
       </header>
     </div>
